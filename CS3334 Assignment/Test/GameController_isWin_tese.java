480f1ad8b0d12a000080c4fc2256300191e2b5bf
krismkt2013 <krismkt2013@gmail.com>
2016-11-20 10:35:39 +0800

    tested the whole isWind() function

--- CS3334 Assignment/Test/GameController_isWin_tese.java
+++ CS3334 Assignment/Test/GameController_isWin_tese.java
@@ -48,7 +48,151 @@ public class GameController_isWin_tese {
 		
 		assertEquals(true, actualResult);
 	}
+
+	@Test
+	public void test_3() {
+		GameBoard gameBoard = this._gameBoard;
+		GameScreenOutputController outputController = this._outputController;
+		gameBoard.initializeGameBoard();
+
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("B", false);	
+		gameBoard.putTokenIntoGameBoard("C", true);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("F", false);
+		gameBoard.putTokenIntoGameBoard("B", false);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("C", false);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		outputController.printGameBoard(gameBoard);
+		
+		this._controller.setGameBoard(gameBoard);
+		boolean actualResult = this._controller.isWin();
+		
+		assertEquals(true, actualResult);
+	}
 	
+	@Test
+	public void test_4() {
+		GameBoard gameBoard = this._gameBoard;
+		GameScreenOutputController outputController = this._outputController;
+		gameBoard.initializeGameBoard();
+
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("B", false);	
+		gameBoard.putTokenIntoGameBoard("C", true);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("F", false);
+		gameBoard.putTokenIntoGameBoard("B", false);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("C", false);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		outputController.printGameBoard(gameBoard);
+		
+		this._controller.setGameBoard(gameBoard);
+		boolean actualResult = this._controller.isWin();
+		
+		assertEquals(false, actualResult);
+	}
 	
+	@Test
+	public void test_5() {
+		GameBoard gameBoard = this._gameBoard;
+		GameScreenOutputController outputController = this._outputController;
+		gameBoard.initializeGameBoard();
+
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("B", false);	
+		gameBoard.putTokenIntoGameBoard("C", true);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("F", false);
+		gameBoard.putTokenIntoGameBoard("B", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", false);
+		gameBoard.putTokenIntoGameBoard("C", false);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("B", false);
+		
+		outputController.printGameBoard(gameBoard);
+		
+		this._controller.setGameBoard(gameBoard);
+		boolean actualResult = this._controller.isWin();
+		
+		assertEquals(true, actualResult);
+	}
+	
+	@Test
+	public void test_6() {
+		GameBoard gameBoard = this._gameBoard;
+		GameScreenOutputController outputController = this._outputController;
+		gameBoard.initializeGameBoard();
+
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("B", false);	
+		gameBoard.putTokenIntoGameBoard("C", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("F", false);
+		gameBoard.putTokenIntoGameBoard("B", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", false);
+		gameBoard.putTokenIntoGameBoard("C", false);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("B", false);
+		
+		outputController.printGameBoard(gameBoard);
+		
+		this._controller.setGameBoard(gameBoard);
+		boolean actualResult = this._controller.isWin();
+		
+		assertEquals(true, actualResult);
+	}
+	
+	@Test
+	public void test_7() {
+		GameBoard gameBoard = this._gameBoard;
+		GameScreenOutputController outputController = this._outputController;
+		gameBoard.initializeGameBoard();
+
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("B", false);	
+		gameBoard.putTokenIntoGameBoard("C", true);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("E", false);
+		gameBoard.putTokenIntoGameBoard("F", false);
+		gameBoard.putTokenIntoGameBoard("B", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", true);
+		gameBoard.putTokenIntoGameBoard("A", false);
+		gameBoard.putTokenIntoGameBoard("C", false);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("D", false);
+		gameBoard.putTokenIntoGameBoard("D", true);
+		gameBoard.putTokenIntoGameBoard("B", false);
+		gameBoard.putTokenIntoGameBoard("C", true);
+		
+		outputController.printGameBoard(gameBoard);
+		
+		this._controller.setGameBoard(gameBoard);
+		boolean actualResult = this._controller.isWin();
+		
+		assertEquals(true, actualResult);
+	}
 
 }
